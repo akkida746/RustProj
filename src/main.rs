006ff0927b192f71fbe9eb14e6afa07263731c7c
Akash <akkida746@gmail.com>
2020-06-20 15:03:40 +0530

    Implementing struct in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod borrowing;
 mod module;
 
 use crate::module::module::printName;
@@ -7,8 +8,33 @@ fn main() {
 
     let name = Employee::Name("Akash".to_string());
     print!("{:?}", name);
-
     printName();
+
+    let data = Data{name:"Akash".to_string(), age:32,optional_num: None};
+    print!("{}, {}", data.name, data.age);
+    print!("{}", data.getAge());
+    let data1 = Data::new();
+    print!("{}", data1.getAge());
+}
+
+struct Data{
+    name: String,
+    age: i32,
+    optional_num: Option<i32>
+}
+
+//Adding methods in struct
+impl Data{
+    fn new() -> Self{
+        Data{
+            name: "Deep".to_string(),
+            age: 31,
+            optional_num: None
+        }
+    }
+    fn getAge(&self) -> i32{
+        self.age
+    }
 }
 
 #[derive(Debug)]
